11c49dd0bec3948f1a104c86430c07b59ea0d1ce
Kamran <kamran.khan.03112002@gmail.com>
2023-08-16 03:04:39 +0530

    Added view own profile page

---
+++ src/components/shared/Gallery.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <div id='gallery' className='py-8 lg:py-24 lg:h-screen flex items-center'>Gallery</div>
+  )
+}
+
+export default Gallery
